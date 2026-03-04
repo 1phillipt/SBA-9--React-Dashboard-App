6a2767d62a553e0a3d5fb57e4000d4a10c1b86bc
Phillip Thapa <1thapat@gmail.com>
2026-03-04 15:31:16 -0500

    added sortTaks by priority duedate and creaedAt function

--- src/utils/taskUtils.ts
+++ src/utils/taskUtils.ts
@@ -1,12 +1,16 @@
-    import type { Task, TaskFilterOptions, TaskFormData } from "../types";
+import type { Task, TaskFilterOptions, TaskFormData } from "../types";
 
-    export function filterTasks(tasks: Task[], filters:TaskFilterOptions){
-        return tasks.filter((tasks) => {
-            const statusMatch = filters.status === "all" || tasks.status === filters.status;
-            const priorityMatch = filters.priority === "all" || tasks.priority === filters.priority;
-            const searchMatch = tasks.title.toLowerCase().includes(filters.search.toLowerCase());
-            return statusMatch && priorityMatch && searchMatch;
-    })
+export function filterTasks(tasks: Task[], filters: TaskFilterOptions) {
+  return tasks.filter((tasks) => {
+    const statusMatch =
+      filters.status === "all" || tasks.status === filters.status;
+    const priorityMatch =
+      filters.priority === "all" || tasks.priority === filters.priority;
+    const searchMatch = tasks.title
+      .toLowerCase()
+      .includes(filters.search.toLowerCase());
+    return statusMatch && priorityMatch && searchMatch;
+  });
 }
 
 //cleans form data and adds Id
@@ -21,8 +25,34 @@ export function createTask(data: TaskFormData): Task {
     createdAt: new Date().toISOString(),
   };
 }
-// export function sortTasks(tasks: Task[], sortBy: "date" | "priority") {
 
-//     const sorted = [...tasks];
+//this will sort data by priority, due date or when data was created
 
-// }
+export function sortTasks(
+  tasks: Task[],
+  sortBy: "priority" | "dueDate" | "createdAt",
+): Task[] {
+  const sortedTasks = [...tasks];
+
+  sortedTasks.sort((a, b) => {
+    if (sortBy === "priority") {
+      const priorityOrder = { high: 3, medium: 2, low: 1 };
+      return priorityOrder[b.priority] - priorityOrder[a.priority];
+    }
+
+    if (sortBy === "dueDate") {
+      if (!a.dueDate) return 1;
+      if (!b.dueDate) return -1;
+
+      return new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime();
+    }
+
+    if (sortBy === "createdAt") {
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+    }
+
+    return 0;
+  });
+
+  return sortedTasks;
+}
